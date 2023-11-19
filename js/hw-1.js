51f75f0cc575cd9c5212c241c196b326222a8241
HepenkoOleksandra <gepenkoalex@gmail.com>
2023-11-19 21:28:05 +0200

    hw-2

---
+++ js/hw-1.js
@@ -0,0 +1,52 @@
+// function getElementWidth(content, padding, border) {
+//     const width = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+//     return width;
+//     // return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// const result = getElementWidth("50px", "8px", "4px");
+// console.log(result);
+
+// const resultOne = getElementWidth("60px", "12px", "8.5px");
+// console.log(resultOne);
+
+// const resultTwo = getElementWidth("200px", "0px", "0px");
+// console.log(resultTwo);
+//======================================
+
+// let price = 0;
+// const subscription = "free";
+
+// if (subscription === "pro") {
+//   price = 50;
+// }
+
+// console.log(price); // 100
+
+// function checkAge(age) {
+//     let adult = undefined;
+//     const old = age;
+//     if (old >= 18) {
+//   adult = "You are an adult";
+//     }
+//     return adult;
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(17));
+// console.log(checkAge(10));
+// console.log(checkAge(30));
+
+// function checkStorage(available, ordered) {
+//   let orderGoods= ordered;
+//   const goods = available;
+//   if (goods < orderGoods) {
+//     console.log("Not enough goods in stock!");
+//   } else {
+//      console.log("Order is processed, our manager will contact you");
+//   }
+  
+// }
